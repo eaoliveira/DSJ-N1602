e03fcaa8b8743f6b9bde22cb5a91c4e6f7b39a93
root <root@aa5d0e18-0176-4192-9acc-f649141629e4>
2012-02-24 21:33:14 +0000

    git-svn-id: file:///Area/Cursos/svn/svn-javades@38 aa5d0e18-0176-4192-9acc-f649141629e4

---
+++ Professor/src/logica/ArrayErro.java
@@ -0,0 +1,11 @@
+package logica;
+
+public class ArrayErro {
+	public static void main(String[] args) {
+		int[] arrayDeItens = new int[100];
+			for (int i = 0; i <= 100; i++) {
+				arrayDeItens[i] = i;
+				System.out.println(arrayDeItens[i]);
+			}
+	}
+}
