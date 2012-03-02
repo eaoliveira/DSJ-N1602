cef2d5a3d0ed0af860de870f05e7112d0b5a7e66
root <root@aa5d0e18-0176-4192-9acc-f649141629e4>
2012-03-02 23:35:57 +0000

    git-svn-id: file:///Area/Cursos/svn/svn-javades@57 aa5d0e18-0176-4192-9acc-f649141629e4

--- Professor/src/objeto/TerceiraTela.java
+++ Professor/src/objeto/TerceiraTela.java
@@ -11,10 +11,14 @@ import java.awt.FlowLayout;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class TerceiraTela extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField tfNome;
+	private JTextField tfIdade;
 
 	/**
 	 * Launch the application.
@@ -24,6 +28,9 @@ public class TerceiraTela extends JFrame {
 			public void run() {
 				try {
 					TerceiraTela frame = new TerceiraTela();
+					// Centraliza a janela no centro da tela
+					frame.setLocationRelativeTo(null);
+					// mostra a tela
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -37,7 +44,7 @@ public class TerceiraTela extends JFrame {
 	 */
 	public TerceiraTela() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 324, 225);
+		setBounds(100, 100, 302, 142);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -48,9 +55,18 @@ public class TerceiraTela extends JFrame {
 		panel.setLayout(new FlowLayout(FlowLayout.RIGHT, 5, 5));
 		
 		JButton btnNewButton = new JButton("Olá");
+		// Define o botao como o padrao para o formulario
+		getRootPane().setDefaultButton(btnNewButton);
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				JOptionPane.showMessageDialog(null, "Como vai?");
+				JOptionPane.showMessageDialog(null, "Como vai, " + 
+						tfNome.getText() + "?\nVocê tem " + 
+						tfIdade.getText() + " anos de idade!");
+				//Limpa os campos 
+				tfNome.setText("");
+				tfIdade.setText("");
+				// posiciona o cursor no campo nome
+				tfNome.requestFocus();
 			}
 		});
 		panel.add(btnNewButton);
@@ -62,6 +78,30 @@ public class TerceiraTela extends JFrame {
 			}
 		});
 		panel.add(btnSair);
+		
+		JPanel panel_1 = new JPanel();
+		FlowLayout flowLayout = (FlowLayout) panel_1.getLayout();
+		flowLayout.setAlignment(FlowLayout.LEFT);
+		contentPane.add(panel_1, BorderLayout.NORTH);
+		
+		JLabel lblNewLabel = new JLabel("Nome");
+		panel_1.add(lblNewLabel);
+		
+		tfNome = new JTextField();
+		panel_1.add(tfNome);
+		tfNome.setColumns(20);
+		
+		JPanel panel_2 = new JPanel();
+		FlowLayout flowLayout_1 = (FlowLayout) panel_2.getLayout();
+		flowLayout_1.setAlignment(FlowLayout.LEFT);
+		contentPane.add(panel_2, BorderLayout.CENTER);
+		
+		JLabel lblNewLabel_1 = new JLabel("Idade");
+		panel_2.add(lblNewLabel_1);
+		
+		tfIdade = new JTextField();
+		panel_2.add(tfIdade);
+		tfIdade.setColumns(5);
 	}
 
 }
