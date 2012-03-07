0a2f4591c92c369efb37e50ae61a4accc67334ee
root <root@aa5d0e18-0176-4192-9acc-f649141629e4>
2012-03-07 01:01:11 +0000

    git-svn-id: file:///Area/Cursos/svn/svn-javades@61 aa5d0e18-0176-4192-9acc-f649141629e4

---
+++ Professor/src/objeto/resposta/Contrato.java
@@ -0,0 +1,37 @@
+package objeto.resposta;
+
+public class Contrato {
+	private int numero;
+	private int ano;
+	private String descricao;
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public int getAno() {
+		return ano;
+	}
+
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	@Override
+	public String toString() {
+		return "numero:" + numero + " ano:" + ano + " descricao:" + descricao;
+	}
+
+}
