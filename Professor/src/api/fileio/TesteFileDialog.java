650e01d1d4f973f4bf208d2a57e3102fb8aac262
root <root@aa5d0e18-0176-4192-9acc-f649141629e4>
2012-03-12 22:39:07 +0000

    git-svn-id: file:///Area/Cursos/svn/svn-javades@73 aa5d0e18-0176-4192-9acc-f649141629e4

---
+++ Professor/src/api/fileio/TesteFileDialog.java
@@ -0,0 +1,15 @@
+package api.fileio;
+
+import java.awt.FileDialog;
+
+import javax.swing.JDialog;
+
+public class TesteFileDialog {
+	public static void main(String[] args) {
+		FileDialog fd = new FileDialog((JDialog) null, 	"Abrir", FileDialog.LOAD);
+		fd.setFile("*.alg");
+		fd.setVisible(true);
+		System.out.println(fd.getDirectory() + fd.getFile());
+		System.exit(0);
+	}
+}
