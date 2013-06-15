031c4a6d50bbacd6766024120c6dcd84abcbe2b3
root <root@aa5d0e18-0176-4192-9acc-f649141629e4>
2013-06-15 19:20:57 +0000

    git-svn-id: file:///Area/Cursos/svn/svn-javades@213 aa5d0e18-0176-4192-9acc-f649141629e4

---
+++ JavaDev/src/Rede/cliente/ServerSimples.java
@@ -0,0 +1,34 @@
+package Rede.cliente;
+
+import java.io.*;
+import java.net.*;
+
+public class ServerSimples  {
+	public static void main(String[] args) {
+		try {
+			while (true) {
+				ServerSocket srv = new ServerSocket(1234);
+				Socket skt = srv.accept();
+
+				InputStreamReader is = new InputStreamReader(
+						skt.getInputStream());
+				BufferedReader in = new BufferedReader(is);
+				PrintWriter out = new PrintWriter(skt.getOutputStream());
+
+				out.println("Bem Vindo");
+				out.flush();
+
+				String txt = in.readLine();
+				while (!txt.equals("SAIR")) {
+					out.println(txt);
+					out.flush();
+
+					txt = in.readLine();
+				}
+				skt.close();
+			}
+		} catch (IOException ex) {
+			System.out.println("Erro durante a conexão: " + ex.getMessage());
+		}
+	}
+}
