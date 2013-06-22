85729d20069d1bb52b891fbcab8caa3c68a619b3
root <root@aa5d0e18-0176-4192-9acc-f649141629e4>
2013-06-22 20:40:54 +0000

    git-svn-id: file:///Area/Cursos/svn/svn-javades@228 aa5d0e18-0176-4192-9acc-f649141629e4

---
+++ JavaDev/src/Rmi/registrosRmi/RegistradorRMI.java
@@ -0,0 +1,23 @@
+package Rmi.registrosRmi;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.rmi.Remote;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class RegistradorRMI {
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(RegistradorRMI.class.getResourceAsStream("registro.config")));
+		
+		Registry rmi = LocateRegistry.createRegistry(1099);
+	
+		String reg = in.readLine();
+		while(reg != null) {
+			String[] campo = reg.split(",");
+			rmi.bind(campo[0], (Remote) Class.forName(campo[1]).newInstance());
+			reg = in.readLine();
+		}
+		in.close();
+	}
+}
