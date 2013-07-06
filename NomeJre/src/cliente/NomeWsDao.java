71adb0cb4ae06652a83a1b783ad5cc3448f13420
root <root@aa5d0e18-0176-4192-9acc-f649141629e4>
2013-07-06 15:58:24 +0000

    git-svn-id: file:///Area/Cursos/svn/svn-javades@254 aa5d0e18-0176-4192-9acc-f649141629e4

---
+++ NomeJre/src/cliente/NomeWsDao.java
@@ -0,0 +1,103 @@
+package cliente;
+
+import java.util.List;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriBuilder;
+
+import xml.Collection;
+import xml.Nome;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.config.ClientConfig;
+import com.sun.jersey.api.client.config.DefaultClientConfig;
+
+public class NomeWsDao {
+	private WebResource service = null;
+
+	public NomeWsDao() {
+		ClientConfig config = new DefaultClientConfig();
+		Client client = Client.create(config);
+		service = client.resource(UriBuilder.fromUri(
+				"http://10.190.39.250:8080/NomeWeb/srv/").build());
+	}
+
+	public List<Nome> listaTodos() throws Error {
+		ClientResponse cli = service.path("bean/all")
+	     .accept(MediaType.APPLICATION_XML)
+	     .get(ClientResponse.class);
+		
+		if(cli.getStatus() != 200)
+			throw new Error("Problemas no acesso aos Dados");
+		
+		return cli.getEntity(Collection.class).getNome();
+	}
+
+	public List<Nome> buscar(String nome) throws Error {
+		ClientResponse cli = service.path("bean/"+nome)
+	     .accept(MediaType.APPLICATION_XML)
+ 	     .header("Content-Type", "text/plain")
+	     .get(ClientResponse.class);
+		
+		if(cli.getStatus() != 200)
+			throw new Error("Problemas no acesso aos Dados");
+		
+		return cli.getEntity(Collection.class).getNome();
+	}
+
+	public Nome buscar(int chave) throws Error {
+		ClientResponse cli = service.path("bean/key/"+chave)
+	     .accept(MediaType.APPLICATION_XML)
+ 	     .header("Content-Type", "text/plain")
+	     .get(ClientResponse.class);
+		
+		if(cli.getStatus() != 200)
+			throw new Error("Problemas no acesso aos Dados");
+		
+		return cli.getEntity(Nome.class);
+	}
+
+	public Nome salvar(Nome obj) throws Error {
+		ClientResponse cli = service.path("bean")
+		 .accept(MediaType.APPLICATION_XML)
+		 .post(ClientResponse.class, obj);
+
+		if(cli.getStatus() != 200)
+			throw new Error("Problemas no acesso aos Dados");
+		
+		return cli.getEntity(Nome.class);
+	}
+
+	public boolean atualizar(Nome obj) throws Error {
+		ClientResponse cli = service.path("bean/update")
+		 .accept(MediaType.APPLICATION_XML)
+		 .post(ClientResponse.class, obj);
+
+		if(cli.getStatus() != 200)
+			return false;
+		else
+		    return true;
+	}
+
+	public void remover(int chave) throws Error {
+		ClientResponse cli = service.path("bean/"+chave)
+	     .accept(MediaType.APPLICATION_XML)
+ 	     .header("Content-Type", "text/plain")
+	     .delete(ClientResponse.class);
+		
+		if(cli.getStatus() != 200)
+			throw new Error("Problemas no acesso aos Dados");
+	}
+
+	public int quantos() throws Error {
+		ClientResponse cli = service.path("bean/count")
+	     .accept(MediaType.APPLICATION_XML).get(ClientResponse.class);
+		
+		if(cli.getStatus() != 200)
+			throw new Error("Problemas no acesso aos Dados");
+		
+		return Integer.parseInt(cli.getEntity(String.class));
+	}
+}
---
+++ NomeWeb/WebContent/script.js
@@ -0,0 +1,98 @@
+// JavaScript de Interface entre o HTML e o RESTFull WebService
+
+function createXHR() {
+	var request = false;
+	try {
+		request = new ActiveXObject('Msxml2.XMLHTTP');
+	} catch (err2) {
+		try {
+			request = new ActiveXObject('Microsoft.XMLHTTP');
+		} catch (err3) {
+			try {
+				request = new XMLHttpRequest();
+			} catch (err1) {
+				request = false;
+			}
+		}
+	}
+	return request;
+}
+
+function todos() {
+	var fname = document.form1.nome.value;
+	var xhr = createXHR();
+	xhr.open("GET", "srv/bean/all", true);
+	xhr.setRequestHeader("Content-Type", "text/plain");
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState == 4) {
+			if (xhr.status != 404) {
+				var xml = xhr.responseText;
+				var tbody = "<h2>Registro</h2><form><table border='1'>" +
+				"<tr><th>Del</th><th>ID</th><th>Nome</th><th>Idade</th></tr>";
+				$(xml).find("nome").each(function() {
+					var id = $(this).find("id").text();
+					var nome = $(this).find("primeiroNome").text();
+					var idade = $(this).find("idade").text();
+					tbody += "<tr><td>" +
+							"<input type='button' value='del' onclick='remove("+id+")'/>" +
+							"</td><td>" +
+							+ id + "</td><td>" + nome + "</td><td>"
+							+ idade + "</td></tr>";
+				});
+				tbody += "</table></form>";
+				document.getElementById("zone").innerHTML = tbody;
+			} else {
+				document.getElementById("zone").innerHTML = fname
+						+ " n&atilde;o encontrado";
+			}
+		}
+	};
+	xhr.send(null);
+}
+
+function remove(chave) {
+	var xhr = createXHR();
+	xhr.open("DELETE", "srv/bean/" + chave, true);
+	xhr.setRequestHeader("Content-Type", "text/plain");
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState == 4) {
+			if (xhr.status != 404) {
+				document.form1.idade.value = "";
+				document.form1.nome.value = "";
+
+				todos();
+			} else {
+				document.getElementById("zone").innerHTML = fname
+						+ " n&atilde;o encontrado";
+			}
+		}
+	};
+	xhr.send(null);
+}
+
+function salva() {
+	var idade = document.form1.idade.value;
+	var nome = document.form1.nome.value;
+	var reg =  
+	  "<nome>" + 
+	    "<id></id>" + 
+	    "<idade>" + idade + "</idade>" + 
+	    "<primeiroNome>" + nome + "</primeiroNome>" + 
+	   "</nome>";
+	var xhr = createXHR();
+	xhr.open("POST", "srv/bean/", true);
+	xhr.setRequestHeader("Content-Type", "application/xml");
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState == 4) {
+			if (xhr.status != 404) {
+				document.form1.idade.value = "";
+				document.form1.nome.value = "";
+
+				todos();
+			} else {
+				document.getElementById("zone").innerHTML = "<h2>Erro</h2>";
+			}
+		}
+	};
+	xhr.send(reg);
+}
