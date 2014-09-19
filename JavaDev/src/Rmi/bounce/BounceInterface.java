a8201c132ec922a2ee0e9b8aa4d1cf159a2f35c8
root <root@aa5d0e18-0176-4192-9acc-f649141629e4>
2014-09-19 21:30:10 +0000

    git-svn-id: file:///Area/Cursos/svn/svn-javades@324 aa5d0e18-0176-4192-9acc-f649141629e4

--- JavaDev/src/Rmi/bounce/BounceInterface.java
+++
@@ -1,9 +0,0 @@
-package Rmi.bounce;
-
-import java.awt.Color;
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface BounceInterface extends Remote {
-  public void createBall(Color c) throws RemoteException;
-}
--- JavaDev/src/Rmi/objetoServer/ObjetoInterface.java
+++
@@ -1,8 +0,0 @@
-package Rmi.objetoServer;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface ObjetoInterface extends Remote{
-  public void salva(Object obj) throws RemoteException;
-}
