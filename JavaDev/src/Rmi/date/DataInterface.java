f60a0c15f39a3a3fd1a7cca475dbcd6f52a2b24d
root <root@aa5d0e18-0176-4192-9acc-f649141629e4>
2015-06-06 13:20:02 +0000

    git-svn-id: file:///Area/Cursos/svn/svn-javades@533 aa5d0e18-0176-4192-9acc-f649141629e4

---
+++ JavaDev/src/Rmi/date/DataInterface.java
@@ -0,0 +1,9 @@
+package Rmi.date;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.Date;
+
+public interface DataInterface extends Remote {
+  public Date getDate() throws RemoteException;
+}
