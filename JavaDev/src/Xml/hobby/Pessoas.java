4002158b1bbf1cf69ce02dc4c7cf621db5047fac
root <root@aa5d0e18-0176-4192-9acc-f649141629e4>
2015-06-13 20:05:26 +0000

    git-svn-id: file:///Area/Cursos/svn/svn-javades@556 aa5d0e18-0176-4192-9acc-f649141629e4

---
+++ JavaDev/src/Xml/hobby/Pessoas.java
@@ -0,0 +1,62 @@
+//
+// Este arquivo foi gerado pela Arquitetura JavaTM para Implementação de Referência (JAXB) de Bind XML, v2.2.8-b130911.1802 
+// Consulte <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
+// Todas as modificações neste arquivo serão perdidas após a recompilação do esquema de origem. 
+// Gerado em: 2015.06.13 às 04:45:13 PM BRT 
+//
+
+
+package Xml.hobby;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "pessoa"
+})
+@XmlRootElement(name = "pessoas")
+public class Pessoas {
+
+    @XmlElement(required = true)
+    protected List<Pessoa> pessoa;
+
+    /**
+     * Gets the value of the pessoa property.
+     * 
+     * <p>
+     * This accessor method returns a reference to the live list,
+     * not a snapshot. Therefore any modification you make to the
+     * returned list will be present inside the JAXB object.
+     * This is why there is not a <CODE>set</CODE> method for the pessoa property.
+     * 
+     * <p>
+     * For example, to add a new item, do as follows:
+     * <pre>
+     *    getPessoa().add(newItem);
+     * </pre>
+     * 
+     * 
+     * <p>
+     * Objects of the following type(s) are allowed in the list
+     * {@link Pessoa }
+     * 
+     * 
+     */
+    public List<Pessoa> getPessoa() {
+        if (pessoa == null) {
+            pessoa = new ArrayList<Pessoa>();
+        }
+        return this.pessoa;
+    }
+
+}
