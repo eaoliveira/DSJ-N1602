57c5e3a75057f7dacd56603ccacf18690186e2d3
root <root@aa5d0e18-0176-4192-9acc-f649141629e4>
2016-01-30 12:48:40 +0000

    git-svn-id: file:///Area/Cursos/svn/svn-javades@677 aa5d0e18-0176-4192-9acc-f649141629e4

---
+++ JavaDev/src/Rede/exemplos/ComoAcessar.java
@@ -0,0 +1,19 @@
+package Rede.exemplos;
+
+import javax.swing.JOptionPane;
+
+public class ComoAcessar {
+	{
+	
+	// Criar um Socket para conectar no endereço 10.84.144.250 na porta 1234
+	
+	// criar o Socket
+	// obter os canais de leitura e gravação
+	// criar um laço para
+	  // ler a mensagem do servidor
+	  //  se a mensagem for diferente de SAIR
+		String msg = JOptionPane.showInputDialog("Entre com a mensagem");
+	  // gravar um texto no servidor
+	// fechar o socket
+	}
+}
