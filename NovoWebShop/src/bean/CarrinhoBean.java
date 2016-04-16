bd464b360434babb4e1e6cddb18da7d8a5b996a7
root <root@aa5d0e18-0176-4192-9acc-f649141629e4>
2016-04-16 13:51:36 +0000

    git-svn-id: file:///Area/Cursos/svn/svn-javades@747 aa5d0e18-0176-4192-9acc-f649141629e4

---
+++ NovoWebShop/src/bean/CarrinhoBean.java
@@ -0,0 +1,54 @@
+package bean;
+
+import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+import javax.faces.model.ListDataModel;
+import javax.servlet.http.HttpSession;
+
+import modelo.Item;
+import dao.CarrinhoDao;
+import dao.ShopException;
+
+@ManagedBean(name = "carrinho")
+@SessionScoped
+public class CarrinhoBean {
+	@EJB
+	private CarrinhoDao dao;
+	private ListDataModel<Item> lista;
+
+	public ListDataModel<Item> getLista() {
+		lista = new ListDataModel<Item>(dao.getLista(getSessionId()));
+		return lista;
+	}
+
+	public void setLista(ListDataModel<Item> lista) {
+		this.lista = lista;
+	}
+
+	public void removeItem(ActionEvent ev) {
+		FacesContext ctx = FacesContext.getCurrentInstance();
+		try {
+			String codigo = (String) ev.getComponent().getAttributes()
+					.get("codigo");
+			dao.removeItem(codigo);
+		} catch (ShopException ex) {
+			ctx.addMessage("form:msg", new FacesMessage(ex.getMessage()));
+		}
+	}
+
+	public double getTotal() {
+		return dao.getTotal(getSessionId());
+	}
+
+	private String getSessionId() {
+		FacesContext ctx = FacesContext.getCurrentInstance();
+		HttpSession session = (HttpSession) ctx.getExternalContext()
+				.getSession(false);
+		String sessionId = session.getId(); // id da sessao
+		return sessionId;
+	}
+}
