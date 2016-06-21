bf0d67a64b96704efef6b1785373518537452979
root <root@aa5d0e18-0176-4192-9acc-f649141629e4>
2016-06-21 22:57:38 +0000

    git-svn-id: file:///Area/Cursos/svn/svn-javades@804 aa5d0e18-0176-4192-9acc-f649141629e4

---
+++ ServletProjeto3/src/servlets/beans4/ValidaDados.java
@@ -0,0 +1,45 @@
+package servlets.beans4;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import servlets.libs.WebUtilities;
+
+@WebServlet("/jsps/ValidaDados")
+public class ValidaDados extends javax.servlet.http.HttpServlet implements
+    javax.servlet.Servlet {
+  static final long serialVersionUID = 1L;
+
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+  	
+    String nome = request.getParameter("nome");
+    String idade = request.getParameter("idade");
+
+	String header = WebUtilities.loadFile(this, "/resources/header.html");
+	String footer = WebUtilities.loadFile(this, "/resources/footer.html");
+	
+    PrintWriter out = response.getWriter();
+    
+    if(nome.isEmpty() || idade.isEmpty()) {
+    	header = header.replaceFirst("TITULO", "Erro");
+    	out.print(header +"<p>Alguns dados não foram informados</p>" + footer);
+    } else {
+    	header = header.replaceFirst("TITULO", "Dados Digitados");
+    	out.print(header +
+    	  "<p><b>Nome</b>: " + nome + "</p>\n" +
+          "<p><b>Idade</b>: " + idade + "</p>\n" +
+          footer);
+    }
+  }
+  
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    doGet(request, response);
+  }
+}
